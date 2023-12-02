9c67ff95b7a11561403c673f4c061f3845cc8bcb
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2023-12-03 04:09:21 +0900

    Add avatar to Profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -12,6 +12,8 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
+  before_destroy :purge_attached_file
+
   enum :prefecture, %i[unselected
                        hokkaido
                        aomori iwate miyagi akita yamagata fukushima
@@ -22,4 +24,14 @@ class Profile < ApplicationRecord
                        tokushima kagawa ehime kouchi
                        fukuoka saga nagasaki kumamoto oita miyazaki kagoshima okinawa
                        outside_japan], prefix: true
+
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
+
+  private
+
+  def purge_attached_file
+    avatar.purge if avatar.attached?
+  end
 end
