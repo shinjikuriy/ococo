47731964c392094ec8e8b179c56a0a5dd8ccec74
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2024-01-03 06:12:21 +0900

    Create profile update page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,24 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+
+  def edit
+    @profile = Profile.find_by(user_id: params[:user_id])
+  end
+
+  def update
+    @profile = Profile.find_by(user_id: params[:user_id])
+
+    if @profile.update(profile_params)
+      flash[:success] = t('.profile_update_succeeded')
+      redirect_to show_user_url(params[:user_id])
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:display_name, :prefecture, :description, :x_username, :ig_username)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/:id', to: 'users#show', as: 'show_user'
+
+  get 'users/:user_id/edit', to: 'profiles#edit', as: 'edit_profile'
+  put 'users/:user_id/edit', to: 'profiles#update', as: 'update_profile'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
