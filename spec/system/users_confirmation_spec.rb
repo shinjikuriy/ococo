57b46f998ae62cc6972027c61306cdd1af85d1f9
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2024-01-15 14:14:17 +0900

    Test user confirmation

---
+++ spec/system/users_confirmation_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "UsersConfirmation", type: :system do
+  describe 'user confirmation' do
+    context 'when user is registered but not confirmed' do
+      let!(:user){ create(:user) }
+      before { visit new_user_confirmation_path }
+
+      specify 'user can confirm account' do
+        fill_in 'user[email]', with: user.email
+        click_button 'commit'
+        expect(page).to have_current_path new_user_session_path
+        expect(page).to have_selector 'div.alert-success', text: t('users.confirmations.send_instructions')
+
+        # a confirmation e-mail should have been sent
+
+        visit user_confirmation_url(confirmation_token: user.confirmation_token)
+        expect(page).to have_current_path new_user_session_path
+        expect(page).to have_selector 'div.alert-success', text: t('users.confirmations.confirmed')
+      end
+
+      context 'when email is invalid' do
+        specify 'an alert appears' do
+          fill_in 'user[email]', with: 'hogehoge@example.com'
+          click_button 'commit'
+          expect(page).to have_selector 'div.alert-warning', text: User.human_attribute_name(:email).concat(t('errors.messages.not_found'))
+        end
+      end
+    end
+  end
+end
