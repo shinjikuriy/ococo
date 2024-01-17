5bde3bcbed3191249a4526a8417057898a4f9f4a
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2024-01-18 00:55:41 +0900

    Test password_change instruction email

--- spec/mailers/devise_mailer_spec.rb
+++ spec/mailers/devise_mailer_spec.rb
@@ -1,15 +1,21 @@
 require "rails_helper"
 
 RSpec.describe Devise::Mailer, type: :mailer do
-  describe 'email changed notification email' do
+  xdescribe 'confirmation_instructions email'
+
+  xdescribe 'reset_password_instructions email'
+
+  xdescribe 'unlock_instructions email'
+
+  describe 'email_changed notification email' do
     let(:user) { create(:user) }
     let(:mail) { Devise::Mailer.email_changed(user) }
 
-    it "sends a notification email to theuser's email address" do
+    it "sends a notification email to the user's email address" do
       expect(mail.to).to eq [user.email]
     end
 
-    it "sends from the email address for transmission" do
+    it 'sends from the email address for transmission' do
       expect(mail.from).to eq ['noreply@ococo.net']
     end
 
@@ -26,5 +32,30 @@ RSpec.describe Devise::Mailer, type: :mailer do
     end
   end
 
-  describe 'password change instruction email'
+  xdescribe 'password_change instruction email' do
+    let(:user) { create(:user) }
+    let(:mail) { Devise::Mailer.password_change(user) }
+
+    it "sends a instruction email to the user's email address" do
+      expect(mail.to).to eq [user.email]
+    end
+
+    it 'sends from the email address for transmission' do
+      expect(mail.from).to eq ['noreply@ococo.net']
+    end
+
+    it 'sends with the correct subject' do
+      expect(mail.subject).to eq t('users.mailer.reset_password_instruction.subject')
+    end
+
+    it 'greets the user by email' do
+      expect(mail.body).to have_text t('users.mailer.reset_password_instruction.greeting', recipient: user.email)
+    end
+
+    it 'has appropriate body text', :aggregate_failures do
+      expect(mail.body).to have_text t('users.mailer.reset_password_instruction.instruction')
+      expect(mail.body).to have_text t('users.mailer.reset_password_instruction.instruction_2')
+      expect(mail.body).to have_text t('users.mailer.reset_password_instruction.instruction_3')
+    end
+  end
 end
