96ce49878f8bfeb117241f22a01dab436f28ca73
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2024-01-28 04:00:59 +0900

    Show pickles using kaminari pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
     if @user.nil?
       flash.now[:warning] = t('errors.messages.page_not_found')
       render 'static_pages/home', status: :not_found
+    else
+      @pickles = @user.pickles.order(updated_at: :desc).page params[:page]
     end
   end
 end
