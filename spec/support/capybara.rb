3c870fd24102718846fa4a346c184ef8dfb58eef
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2024-02-08 01:02:44 +0900

    update rspec-rails

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,21 +3,18 @@ require 'selenium-webdriver'
 
 Capybara.register_driver :remote_chrome do |app|
   url = ENV['SELENIUM_DRIVER_URL']
-  capabilities = ::Selenium::WebDriver::Remote::Capabilities.chrome(
-    'goog:chromeOptions' => {
-      'args' => [
-        'no-sandbox',
-        'headless',
-        'disable-gpu',
-        'window-size=1680,1050'
-      ]
-    }
-  )
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('no-sandbox')
+  options.add_argument('headless=new')
+  options.add_argument('disable-gpu')
+  options.add_argument('disable-dev-shm-usage')
+  options.add_argument('remote-debugging-port=9222')
+  options.add_argument('window-size=1400, 1400')
 
   Capybara::Selenium::Driver.new(app,
                                  browser: :remote,
                                  url: url,
-                                 capabilities: capabilities)
+                                 capabilities: options)
 end
 
 RSpec.configure do |config|
@@ -26,9 +23,9 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :system, js: true) do
+    driven_by :remote_chrome
     Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
     Capybara.server_port = 4444
     Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
-    driven_by :remote_chrome
   end
 end
