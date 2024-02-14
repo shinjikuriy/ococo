f96d77cf03a603b537e8455a37881d64d8469542
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2024-02-14 23:29:55 +0900

    Implement Journal dependencies

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,3 +1,4 @@
 class Journal < ApplicationRecord
   belongs_to :pickle
+  has_one :user, through: :pickle
 end
--- spec/models/journal_spec.rb
+++ spec/models/journal_spec.rb
@@ -6,25 +6,21 @@ RSpec.describe Journal, type: :model do
     let!(:pickle) { user.pickles.create(attributes_for(:pickle)) }
 
     specify 'a pickle can have many journals' do
-      journal = Journal.create(pickle_id: pickle.id, body: 'ほげほげ')
+      Journal.create(pickle_id: pickle.id, body: 'ほげほげ')
       Journal.create(pickle_id: pickle.id, body: 'ふがふが')
 
       expect(pickle.journals.size).to eq 2
-      expect(pickle.journals.first.body).to eq 'ほがほが'
+      expect(pickle.journals.first.body).to eq 'ほげほげ'
       expect(pickle.journals.second.body).to eq 'ふがふが'
-
-      expect(pickle.journals.first).to be journal
     end
 
     specify 'a user can have many journals' do
-      journal = Journal.create(pickle_id: pickle.id, body: 'ほげほげ')
+      Journal.create(pickle_id: pickle.id, body: 'ほげほげ')
       Journal.create(pickle_id: pickle.id, body: 'ふがふが')
 
       expect(user.journals.size).to eq 2
       expect(user.journals.first.body).to eq 'ほげほげ'
       expect(user.journals.second.body).to eq 'ふがふが'
-
-      expect(user.jounals.first).to be journal
     end
   end
 
