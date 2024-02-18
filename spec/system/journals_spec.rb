d5915f0cc005031a60117b79dd5f03d7e88c395a
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2024-02-19 00:41:38 +0900

    Test journals#destroy

--- spec/system/journals_spec.rb
+++ spec/system/journals_spec.rb
@@ -87,7 +87,37 @@ RSpec.describe "Journals", type: :system do
     end
   end
 
-  describe 'UPDATE'
+  describe 'DELETE' do
+    let!(:user) { create(:user, :confirmed) }
+    let!(:pickle) { user.pickles.create(attributes_for(:pickle)) }
+    let!(:journal) { pickle.journals.create(attributes_for(:journal)) }
 
-  describe 'DELETE'
+    specify 'user can delete own journal', js: true do
+      visit user_path(user)
+      within '#journals' do
+        expect do
+          page.accept_confirm do
+            click_on t('journals.shared.destroy_journal')
+          end
+        end.to change { Journal.count }.by -1
+      end
+      expect(page).to have_selector('div.alert-success'), text: t('journals.shared.destroyed_journal')
+      expect(find('#journals')).not_to have_text journal.body
+    end
+
+    specify "user cannot delete others' journal" do
+      lena = create(:user_lena)
+      pickle_lena = lena.pickles.create(attributes_for(:pickle_kabu))
+      journal_lena = pickle_lena.journals.create(attributes_for(:journal))
+
+      visit user_path(lena)
+      expect(find('#journals')).to have_text journal_lena.body
+      expect(find('#journals')).not_to have_text t('journals.shared.destroy_journal')
+      within '.pickle-section' do
+        click_link pickle_lena.name
+      end
+      expect(find('#journals')).to have_text journal_lena.body
+      expect(find('#journals')).not_to have_text t('journals.shared.destroy_journal')
+    end
+  end
 end
