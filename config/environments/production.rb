85339676e4970637ba1780e54d93ac9607777a9a
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2024-03-03 05:57:41 +0900

    Redo ActionMailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,18 +75,21 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'ococo.onrender.com' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'gmail.com',
-    username: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_APP_PASSWORD'],
-    authentication: 'plain',
+    address:         'smtp.gmail.com',
+    port:            587,
+    domain:          'gmail.com',
+    user_name:       ENV['GMAIL_ADDRESS'],
+    password:        ENV['GMAIL_APP_PASSWORD'],
+    authentication:  'plain',
     enable_starttls: true,
-    open_timeout: 5,
-    read_timeout: 5 }
+    open_timeout:    5,
+    read_timeout:    5 }
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
