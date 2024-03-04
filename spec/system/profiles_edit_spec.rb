e22f827b39f7601b05bfb38b0ad4d4b9f37cd92b
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2024-03-05 01:42:49 +0900

    Test validation fails

--- spec/system/profiles_edit_spec.rb
+++ spec/system/profiles_edit_spec.rb
@@ -52,5 +52,49 @@ RSpec.describe "ProfilesEdit", type: :system do
         expect(page).to have_selector("img[src$='avatar_cat.png']")
       end
     end
+
+    context 'when information is invalid' do
+      specify 'an alert appears when display_name is empty' do
+        fill_in 'profile_display_name', with: ''
+        click_button 'commit'
+        expect(page).to have_selector 'div.alert-warning', text: Profile.human_attribute_name(:display_name).concat(t('errors.messages.empty'))
+      end
+
+      specify 'an alert appears when display_name is more than 30 characters' do
+        fill_in 'profile_display_name', with: 'a' * 31
+        click_button 'commit'
+        expect(page).to have_selector 'div.alert-warning', text: Profile.human_attribute_name(:display_name).concat(t('errors.messages.too_long', count: 30))
+      end
+
+      specify 'an alert appears when description is more than 160 characters' do
+        fill_in 'profile_description', with: 'a' * 161
+        click_button 'commit'
+        expect(page).to have_selector 'div.alert-warning', text: Profile.human_attribute_name(:description).concat(t('errors.messages.too_long', count: 160))
+      end
+
+      specify 'an alert appears when x_username contains invalid characters' do
+        fill_in 'profile_x_username', with: 'luke.x'
+        click_button 'commit'
+        expect(page).to have_selector 'div.alert-warning', text: Profile.human_attribute_name(:x_username).concat(t('errors.messages.invalid_username_format'))
+      end
+
+      specify 'an alert appears when ig_username contains invalid characters' do
+        fill_in 'profile_ig_username', with: 'luke_ig!'
+        click_button 'commit'
+        expect(page).to have_selector 'div.alert-warning', text: Profile.human_attribute_name(:ig_username).concat(t('errors.messages.invalid_ig_username_format'))
+      end
+
+      specify 'an alert appears when avatar file is not an image' do
+        attach_file 'profile_avatar', "#{Rails.root}/spec/fixtures/joyo.tsv"
+        click_button 'commit'
+        expect(page).to have_selector 'div.alert-warning', text: Profile.human_attribute_name(:avatar).concat(t('errors.messages.invalid_content_type'))
+      end
+
+      specify 'an alert appears when avatar file is more than 2MB' do
+        attach_file 'profile_avatar', "#{Rails.root}/spec/fixtures/too_large_file.jpg"
+        click_button 'commit'
+        expect(page).to have_selector 'div.alert-warning', text: Profile.human_attribute_name(:avatar).concat(t('errors.messages.file_size_is_too_large'))
+      end
+    end
   end
 end
