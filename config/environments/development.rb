0a78e07ec57d8426abc2cfbb633df2d29ee72707
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2024-03-06 07:48:12 +0900

    Use letter_opener in development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,22 +36,25 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:         'smtp.gmail.com',
-    port:            587,
-    domain:          'gmail.com',
-    user_name:       ENV['GMAIL_ADDRESS'],
-    password:        ENV['GMAIL_APP_PASSWORD'],
-    authentication:  'plain',
-    enable_starttls: true,
-    open_timeout:    5,
-    read_timeout:    5 }
+
+  config.action_mailer.delivery_method = :letter_opener
+
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:         'smtp.gmail.com',
+  #   port:            587,
+  #   domain:          'gmail.com',
+  #   user_name:       ENV['GMAIL_ADDRESS'],
+  #   password:        ENV['GMAIL_APP_PASSWORD'],
+  #   authentication:  'plain',
+  #   enable_starttls: true,
+  #   open_timeout:    5,
+  #   read_timeout:    5 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -85,6 +88,4 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
-
-  config.active_storage.service = :local
 end
