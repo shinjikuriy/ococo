e56215001033e4e8b0db001ed4a414a29d89f486
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2024-03-11 23:50:37 +0900

    Show page title and app title on browser title bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  def full_title(page_title='')
+    base_title = "#{t('app.app_name')} - #{t('app.app_description')}"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
   # deviseのalertをbootstrapのclassに対応させる
   def bootstrap_alert(key)
     case key
